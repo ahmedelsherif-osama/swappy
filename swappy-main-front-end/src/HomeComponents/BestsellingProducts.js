9a50c2dd3b29997e6ef544e9a6ac216357b91372
ammarafridi-max <64968582+ammarafridi-max@users.noreply.github.com>
2022-12-21 14:13:06 +0400

    Home Components updated

--- swappy-main-front-end/src/HomeComponents/BestsellingProducts.js
+++ swappy-main-front-end/src/HomeComponents/BestsellingProducts.js
@@ -2,6 +2,33 @@ import ProductCard from "../ProductCard"
 
 function BestsellingProducts(){
 
+    const products = [
+        {
+            name:"Sofa Bed",
+            price: "2000",
+            location: "Al Mamzar, Dubai",
+            img:"./images/img3.png"
+        },
+        {
+            name:"Double Bed",
+            price: "3000",
+            location: "JLT, Dubai",
+            img:"./images/biophilic-design.png"
+        },
+        {
+            name:"Office Desk",
+            price: "1500",
+            location: "Muwaileh, Sharjah",
+            img:"./images/sustainable-chair.png"
+        },
+        {
+            name:"TV Table",
+            price: "1900",
+            location: "Al Khan, Dubai",
+            img:"./images/img3.png"
+        },
+    ]
+
     return(
         <section className="col-lg-10 col-11 primary-section margin-y-5  mx-auto">
 
@@ -9,10 +36,10 @@ function BestsellingProducts(){
             <div className="heading-border"></div>
 
             <div className="col-lg-12 col-12 col-md-10 row mx-auto">
-                <ProductCard productName="Sofa Bed" productPrice="2,000" productLocation="Al Mamzar, Dubai" />
-                <ProductCard productName="Sofa Bed" productPrice="2,000" productLocation="Al Mamzar, Dubai" />
-                <ProductCard productName="Sofa Bed" productPrice="2,000" productLocation="Al Mamzar, Dubai" />
-                <ProductCard productName="Sofa Bed" productPrice="2,000" productLocation="Al Mamzar, Dubai" />
+                <ProductCard name={products[0].name} price={products[0].price} location={products[0].location} img={products[0].img} />
+                <ProductCard name={products[1].name} price={products[1].price} location={products[1].location} img={products[1].img} />
+                <ProductCard name={products[2].name} price={products[2].price} location={products[2].location} img={products[2].img} />
+                <ProductCard name={products[3].name} price={products[3].price} location={products[3].location} img={products[3].img} />
             </div>
 
         </section>
--- swappy-main-front-end/src/HomeComponents/Testimonials.js
+++ swappy-main-front-end/src/HomeComponents/Testimonials.js
@@ -6,14 +6,44 @@ function Testimonials(){
         "color":"#EEEEEE"
     }
 
-    let borderStyle = {
-        "border-top-color":"#393E46"
+    let testimonialsImg = {
+        "background": "linear-gradient(90deg,rgba(0,0,0,0),rgba(126, 217, 87, 0)), url(/images/testimonial-img.png)",
+        "background-repeat": "no-repeat",
+        "background-position": "right", 
+        "background-size":"cover",
+        "border-radius": "0",
+        "height":"450px",
+        "margin":"2%"
+    }
+
+    let testimonialsContent ={
+        "margin-top":"3%",
+        "margin-bottom":"-3%"
     }
 
     return(
         <section style={sectionStyle}>
-            <h2 className="text-center">Happy Customers</h2>
-            <div className="heading-border" style={borderStyle}></div>
+
+            <div className="col-lg-9 mx-auto row">
+
+                <div className="col-lg-6 bg-light">
+                    <div className="container-fluid mx-auto" style={testimonialsImg}>
+                    </div>
+                </div>
+
+                <div className="col-lg-6 bg-light padding-y-3" style={testimonialsContent}>
+
+                    <div className="text-center">
+                        <h3 className="text-dark">Happy Customers!</h3>
+                    </div>
+
+                    <div>
+
+                    </div>
+
+                </div>
+            </div>
+
         </section>
     )
 }
